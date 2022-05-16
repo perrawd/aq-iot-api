83fac39f9ededeb0bcd37bb7846fbf31b0e08655
Per Rawdin <pr222iw@student.lnu.se>
2022-05-16 15:04:32 +0200

    Initial commit

---
+++ src/main/java/com/aq/aqiotapi/AqIotApiApplication.java
@@ -0,0 +1,13 @@
+package com.aq.aqiotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqIotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqIotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aq/aqiotapi/AqIotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aq.aqiotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqIotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
